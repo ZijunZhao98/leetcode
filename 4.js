642601cee7a2cf9d0b98f4f993259ee92cd9a2f4
ZijunZhao98 <zijun_zhao1998@163.com>
2018-05-24 01:31:38 -0500

    Hard - 两个排序数组的中位数
    based on this solution: https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2471/very-concise-ologminmn-iterative-solution-with-detailed-explanation

--- 4.js
+++ 4.js
@@ -24,10 +24,38 @@ var findMedianSortedArrays = function(nums1, nums2) {
     }
     
     //if neither of the arrays are empty
-    findMedian(nums1, nums2, 0, 0, nums1.length, nums2.length);
+    return findMedian(nums1, nums2);
     
 };
 
-var findMedian(a, b, a_begin, a_end, b_begin, b_end){
+var findMedian = function(a, b){
+    var N1 = a.length;
+    var N2 = b.length;
+    if (N1 < N2) return findMedian(b, a);
     
-}
+    var lo = 0, hi = N2 * 2;
+    while (lo <= hi) {
+        var mid2 = Math.floor((lo + hi) / 2);  
+        var mid1 = N1 + N2 - mid2; 
+        
+        var INT_MAX = Number.MAX_SAFE_INTEGER;
+        var INT_MIN = Number.MIN_SAFE_INTEGER;
+        
+        var L1 = (mid1 == 0) ? INT_MIN : a[Math.floor((mid1-1)/2)];	
+        var L2 = (mid2 == 0) ? INT_MIN : b[Math.floor((mid2-1)/2)];
+        var R1 = (mid1 == N1 * 2) ? INT_MAX : a[Math.floor((mid1)/2)];
+        var R2 = (mid2 == N2 * 2) ? INT_MAX : b[Math.floor((mid2)/2)];
+        
+        
+        if (L1 > R2){
+            lo = mid2 + 1;
+        } else if (L2 > R1){
+            hi = mid2 - 1;
+        } else{
+            return (Math.max(L1,L2) + Math.min(R1, R2)) / 2;
+        }
+              
+    }
+    return -1;
+    
+};
